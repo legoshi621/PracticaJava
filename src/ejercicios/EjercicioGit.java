176cd56e9155836a8f635da5dac6afb27bb3129a
Alan Edenilson Campos Sanchez <cs25001@esfe.agape.edu.sv>
2026-05-21 11:36:43 -0600

    primer cambio

--- src/ejercicios/EjercicioGit.java
+++ src/ejercicios/EjercicioGit.java
@@ -1,6 +1,6 @@
 package ejercicios;
 public class EjercicioGit {
     public static void main(String[] args) {
-        System.out.println("Primer cambio en rama camila-dev");
+        System.out.println("Primer cambio en rama karla-dev");
     }
 }
